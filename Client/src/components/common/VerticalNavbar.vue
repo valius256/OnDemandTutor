323443278cfdb7071425ed1c76b513af330b9e8b
kisphapha <thanhhung16082003@gmail.com>
2024-06-21 12:44:48 +0700

    Animated ver nav bar

--- Client/src/components/common/VerticalNavbar.vue
+++ Client/src/components/common/VerticalNavbar.vue
@@ -1,60 +1,80 @@
 <template>
     <div>
-        <div class="h-screen bg-blue-950 text-white overflow-y-auto">
-            <div class="text-3xl font-bold py-5 px-5">
-                <span>On</span>
-                <span class="text-blue-600">Demand</span>
-                <span>Tutor</span>
-            </div>
-            <div class="mt-8">
-                <div class="flex flex-col font-bold text-white">
-                    <router-link to="/admin/accounts" class="px-2 py-4 hover:bg-blue-800 ">
-                        <i class="fa fa-users text-2xl mr-2"></i>Quản lý tài khoản
-                    </router-link>
-                    <div class="flex flex-col sub-nav" v-if="$route.path.startsWith('/admin/accounts')">
-                        <router-link to="/admin/accounts/students" class="px-8 py-4 hover:bg-blue-800">
-                            <i class="fa fa-user text-2xl mr-2"></i>  Học sinh
+        <div class="relative">
+            <div class="h-screen bg-blue-950 text-white overflow-y-auto change-width"
+            :class="{'w-[18rem]' : !isCollapse, 'w-16' : isCollapse}">
+                <div class="text-3xl font-bold py-5 px-5" v-if="!isCollapse">
+                    <span>On</span>
+                    <span class="text-blue-600">Demand</span>
+                    <span>Tutor</span>
+                </div>
+                <div class="mt-8">
+                    <div class="flex flex-col font-bold text-white">
+                        <router-link to="/admin/accounts" class="px-2 py-4 hover:bg-blue-800 ">
+                            <i class="fa fa-users text-2xl mr-2"></i>
+                            <span v-if="!isCollapse">Quản lý tài khoản</span>
                         </router-link>
-                        <router-link to="/admin/accounts/tutors" class="px-8 py-4 hover:bg-blue-800">
-                            <i class="fa fa-graduation-cap text-2xl mr-2"></i> Gia sư
+                        <div class="flex flex-col sub-nav" v-if="$route.path.startsWith('/admin/accounts')">
+                            <router-link to="/admin/accounts/students" class="py-4 bg-[#1c2e5f] hover:bg-blue-800" :class="{'px-2' : isCollapse, 'px-8' : !isCollapse}">
+                                <i class="fa fa-user text-2xl mr-2"></i>
+                                <span  v-if="!isCollapse" >Học sinh</span>
+                            </router-link>
+                            <router-link to="/admin/accounts/tutors" class="py-4 bg-[#1c2e5f] hover:bg-blue-800" :class="{'px-2' : isCollapse, 'px-8' : !isCollapse}">
+                                <i class="fa fa-graduation-cap text-2xl mr-2"></i>
+                                <span  v-if="!isCollapse" >Gia sư</span>
+                            </router-link>
+                            <router-link to="/admin/accounts/operators" class=" py-4 bg-[#1c2e5f] hover:bg-blue-800" :class="{'px-2' : isCollapse, 'px-8' : !isCollapse}">
+                                <i class="fa fa-gear text-2xl mr-2"></i>
+                                <span v-if="!isCollapse" >Vận hành</span>
+                            </router-link>
+                        </div>
+                        <router-link to="/admin/subjects" class="px-2 py-4 hover:bg-blue-800">
+                            <i class="fa fa-bookmark text-2xl mr-2"></i>
+                            <span v-if="!isCollapse">Quản lý môn học</span>
                         </router-link>
-                        <router-link to="/admin/accounts/operators" class="px-8 py-4 hover:bg-blue-800">
-                            <i class="fa fa-gear text-2xl mr-2"></i> Vận hành
+                        <router-link to="/admin/blogs" class="px-2 py-4 hover:bg-blue-800">
+                            <i class="fa fa-pencil-square-o text-2xl mr-2"></i>
+                            <span v-if="!isCollapse">Quản lý Blog</span>
                         </router-link>
+                        <router-link to="/admin/faqs" class="px-2 py-4 hover:bg-blue-800">
+                            <i class="fa fa-question-circle text-2xl mr-2"></i>
+                            <span v-if="!isCollapse">Quản lý FAQ</span>
+                        </router-link>
+                        <router-link to="/admin/consultation" class="px-2 py-4 hover:bg-blue-800">
+                            <i class="fa fa-comment-o text-2xl mr-2"></i>
+                            <span v-if="!isCollapse">Yêu cầu tư vấn</span>
+                        </router-link>
+
                     </div>
-                    <router-link to="/admin/subjects" class="px-2 py-4 hover:bg-blue-800">
-                        <i class="fa fa-bookmark text-2xl mr-2"></i>Quản lý môn học
-                    </router-link>
-                    <!-- <router-link to="/admin/classes" class="px-2 py-4 hover:bg-blue-800">
-                        <i class="fa fa-calendar text-2xl mr-2"></i>Quản lý lớp học
-                    </router-link> -->
-                    <router-link to="/admin/blogs" class="px-2 py-4 hover:bg-blue-800">
-                        <i class="fa fa-pencil-square-o text-2xl mr-2"></i>Quản lý Blog
-                    </router-link>
-                    <router-link to="/admin/faqs" class="px-2 py-4 hover:bg-blue-800">
-                        <i class="fa fa-question-circle text-2xl mr-2"></i>Quản lý FAQ
-                    </router-link>
-                    <router-link to="/admin/consultation" class="px-2 py-4 hover:bg-blue-800">
-                        <i class="fa fa-comment-o text-2xl mr-2"></i>Yêu cầu tư vấn
-                    </router-link>
-                    
+                </div>
+                <div class="flex flex-col ">
+                    <button class="px-2 py-4 font-bold text-white text-left hover:bg-blue-800">
+                        <i class="fa fa-sign-out text-2xl mr-2"></i>
+                        <span v-if="!isCollapse">Đăng xuất</span>
+                    </button>
                 </div>
             </div>
-            <div class="flex flex-col ">
-                <button class="px-2 py-4 font-bold text-white text-left hover:bg-blue-800">
-                    <i class="fa fa-sign-out text-2xl mr-2"></i>Đăng xuất
-                </button>
-            </div>
+            <button class="absolute bg-gray-400 z-10 top-1/2 -right-4 w-4 h-32 rounded-r-xl" @click="toggleCollapse">
+                <i v-if="!isCollapse" class="fa fa-mail-reply"></i>
+                <i v-if="isCollapse" class="fa fa-mail-forward"></i>
+            </button>
         </div>
     </div>
 </template>
 
 <script>
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-
 export default {
     name: "VerticalNavbar",
-    components : {FontAwesomeIcon}
+    data() {
+        return {
+            isCollapse: false
+        }
+    },
+    methods: {
+        toggleCollapse() {
+            this.isCollapse = !this.isCollapse
+        }
+    },
 }
 </script>
 
@@ -62,7 +82,12 @@ export default {
 .router-link-active {
     background: #0a0030;
 }
+
 .sub-nav .router-link-active {
     background: #0c0038;
+}
+
+.change-width {
+    transition: width 0.3s ease;
 }
 </style>
--- Client/src/pages/Operators/StudentManagementPage.vue
+++ Client/src/pages/Operators/StudentManagementPage.vue
@@ -1,15 +1,230 @@
 <template>
-    <div class="p-4">
+    
+    <div class="p-4 w-full" @click="setSelectId(0)">
         <div class="text-2xl font-bold">
-            This is a admin student management page
+            Quản lý học sinh
+        </div>
+        <div class="flex justify-end gap-2">
+            <button class="py-2 px-4 bg-slate-500 hover:bg-slate-300 text-white font-bold rounded-lg"
+                @click="handleSearch">
+                <i class="fa fa-search	"></i> Filter
+            </button>
+        </div>
+        <table id="operator-table">
+            <thead>
+                <tr>
+                    <th class="w-1/12">Id</th>
+                    <th class="w-2/12">Tên</th>
+                    <th class="w-2/12">Ảnh</th>
+                    <th class="w-3/12">Dạy môn</th>
+                    <th class="w-2/12">Email</th>
+                    <th class="w-2/12">SDT</th>
+                    <th class="w-2/12">Trạng thái</th>
+                    <th class="w-1/12"></th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="tutor in tutors" :key="tutor.id">
+                    <td>{{ tutor.id }}</td>
+                    <td><button class="font-bold underline text-blue-400">{{ tutor.name }}</button></td>
+                    <td><img :src="tutor.avatar" class="w-24 h-24"></td>
+                    <td>
+                        <div class="flex flex-wrap gap-1" v-html="displaySubjects(tutor.subjects)"></div>
+                    </td>
+                    <td class="break-all">{{ tutor.email }}</td>
+                    <td>{{ tutor.phone }}</td>
+                    <td>
+                        <div :class="getStatusStyle(tutor.status)">{{ tutor.status }}</div>
+                    </td>
+                    <td class="relative">
+                        <button class="p-2 bg-slate-200 hover:bg-slate-400 font-bold rounded-full"
+                            @click.stop="setSelectId(tutor.id)">
+                            <i class="fa fa-ellipsis-h	"></i>
+                        </button>
+                        <div v-if="selectId == tutor.id"
+                            class="absolute right-0 bg-white rounded-lg shadow-lg z-10 w-48 animate-fade-down animate-duration-[400ms] animate-normal font-bold flex flex-col">
+                            <!-- Content of your menu -->
+                                <button class="hover:bg-slate-200 p-2 rounded-t-lg text-left">
+                                    <i class="fa fa-user mr-4"></i>Xem hồ sơ
+                                </button>
+                                <!-- <li class="hover:bg-slate-200 p-2"></li> -->
+                                <button class="hover:bg-slate-200 p-2 rounded-b-lg text-left text-red-500">
+                                    <i class="fa fa-remove mr-4"></i>Đình chỉ
+                                </button>
+                        </div>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+        <div class="flex gap-4 justify-center mt-4" v-if="this.tutors.length > 0">
+            <button @click="movePage(false)">
+                <i class="fa fa-arrow-left text-2xl"></i>
+            </button>
+            <div class="flex gap-2 ">
+                <input class="border p-1 rounded-md w-16" type="number" v-model="currentPage" min="1"
+                    @change="handlePageChange">
+                <div class="p-1"> / {{ this.totalPage }}</div>
+            </div>
+            <button @click="movePage(true)">
+                <i class="fa fa-arrow-right text-2xl"></i>
+            </button>
         </div>
     </div>
 </template>
 
 <script>
 export default {
-    name : "StudentManagementPage"
+    name: "AdminTutorList",
+    data() {
+        return {
+            totalPage: 100,
+            pageSize: 10,
+            currentPage: 1,
+            selectId: 0,
+            isShowPopup: false,
+            tutors: [
+                {
+                    id: 1,
+                    name: "Nguyen Van A",
+                    email: "abc@gmail.com",
+                    phone: "0987654321",
+                    avatar: "/src/assets/noavatar.jpg",
+                    status: "Active",
+                    subjects: [
+                        {
+                            name: "Toán"
+                        },
+                        {
+                            name: "Tiếng Anh"
+                        },
+                        {
+                            name: "Vật Lý"
+                        },
+                    ]
+                },
+                {
+                    id: 2,
+                    name: "Nguyen Van A",
+                    email: "abc@gmail.com",
+                    phone: "0987654321",
+                    avatar: "/src/assets/noavatar.jpg",
+                    status: "Active",
+                    subjects: [
+                        {
+                            name: "Toán"
+                        },
+                        {
+                            name: "Tiếng Anh"
+                        },
+                        {
+                            name: "Vật Lý"
+                        },
+                    ]
+                },
+                {
+                    id: 3,
+                    name: "Nguyen Van A",
+                    email: "abc@gmail.com",
+                    phone: "0987654321",
+                    avatar: "/src/assets/noavatar.jpg",
+                    status: "Fired",
+                    subjects: [
+                        {
+                            name: "Toán"
+                        },
+                        {
+                            name: "Piano, organ"
+                        },
+                    ]
+                },
+                {
+                    id: 4,
+                    name: "Nguyen Van A",
+                    email: "abc@gmail.com",
+                    phone: "0987654321",
+                    avatar: "/src/assets/noavatar.jpg",
+                    status: "Left",
+                    subjects: [
+                        {
+                            name: "Tiếng Nhật"
+                        },
+                        {
+                            name: "Tiếng Anh"
+                        },
+                    ]
+                },
+            ],
+        }
+    },
+    methods: {
+        clearSelectId() {
+            if (this.isShowPopup) {
+                this.selectId = 0
+                this.isShowPopup = false
+            }
+        },
+        setSelectId(id) {
+            if (id == this.selectId) {
+                this.selectId = 0
+            } else {
+                this.selectId = id
+                this.isShowPopup = true
+            }
+        },
+        displaySubjects(subjects) {
+            let color = "gray"
+            let html = ""
+            for (var subject of subjects) {
+                switch (subject.name) {
+                    case "Toán":
+                        color = "border-orange-400"
+                        break;
+                    case "Tiếng Anh":
+                        color = "border-green-400"
+                        break;
+                    case "Tiếng Nhật":
+                        color = "border-pink-400"
+                        break;
+                }
+                var style = `rounded-lg py-2 px-6 border ${color}`
+                html += `<span class="${style}">${subject.name}</span>`
+            }
+            return html
+        },
+        getStatusStyle(status) {
+            let css = "text-center font-bold text-white rounded-lg p-1"
+            switch (status) {
+                case "Active":
+                    return css + " bg-green-400"
+                case "Left":
+                    return css + " bg-gray-400"
+                case "Fired":
+                    return css + " bg-red-400"
+            }
+        },
+        async handlePageChange() {
+            if (this.currentPage > this.totalPage) {
+                this.currentPage = this.totalPage
+            }
+            if (this.currentPage < 1) {
+                this.currentPage = 1
+            }
+            //await this.fetchRegistration(this.currentPage, this.pageSize, this.keyword_name)
+        },
+        async movePage(forward) {
+            if (forward && this.currentPage < this.totalPage) {
+                this.currentPage++
+                await this.handlePageChange()
+            } else if (!forward && this.currentPage > 1) {
+                this.currentPage--
+                await this.handlePageChange()
+            }
+        },
+    },
+    mounted() {
+    }
 }
 </script>
 
-<style></style>
+<style scoped>
+</style>
