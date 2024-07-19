3026d6ad40a252bbf091dc1d621017c523452cce
valius256 <quangphat7a1@gmail.com>
2024-07-19 14:43:22 +0700

    Fix Conflict

--- Server/OnDemandTutor.BusinessLogic/Services/Class/ClassServices.cs
+++ Server/OnDemandTutor.BusinessLogic/Services/Class/ClassServices.cs
@@ -34,9 +34,9 @@ namespace OnDemandTutor.BusinessLogic.Services.Class
                 var classSlots = class_?.Slots.ToList() ?? new List<Models.Models.Slot>();
                 if( classSlots.Any())
                 {
-                    result.StartTime = classSlots[0].StartTime;
+                result.StartTime = classSlots[0].StartTime;
                     result.EndTime = classSlots[classSlots.Count-1].EndTime;
-                }
+            }
             }
             return mappedResult;
         }
@@ -65,7 +65,7 @@ namespace OnDemandTutor.BusinessLogic.Services.Class
                 var class_ = pagedResult.Items.FirstOrDefault(x => x.Id == result.Id);
                 var classSlots = class_?.Slots.ToList() ?? new List<Models.Models.Slot>();
                 if (classSlots.Any())
-                {
+        {
                     result.StartTime = classSlots[0].StartTime;
                     result.EndTime = classSlots[classSlots.Count - 1].EndTime;
                 }
